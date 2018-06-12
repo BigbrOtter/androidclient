d77bcd65e448f6c186686b82c7fb672fe95f4e6b
Martijn Heeffer <Martijn.heeffer@hotmail.com>
2018-06-12 13:52:54 +0200

    AlertHelper

--- app/src/main/java/nl/bigbrotter/androidclient/Controllers/MainActivity.java
+++ app/src/main/java/nl/bigbrotter/androidclient/Controllers/MainActivity.java
@@ -2,14 +2,29 @@ package nl.bigbrotter.androidclient.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import nl.bigbrotter.androidclient.Helpers.AlertHelper;
 import nl.bigbrotter.androidclient.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bAlertTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bAlertTest = findViewById(R.id.activity_main_btn_alert_test);
+
+        bAlertTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertHelper.error(MainActivity.this, "Stream werkt voor geen meter maatie!");
+            }
+        });
+
     }
 }
--- app/src/main/java/nl/bigbrotter/androidclient/Helpers/AlertHelper.java
+++ app/src/main/java/nl/bigbrotter/androidclient/Helpers/AlertHelper.java
@@ -1,8 +1,98 @@
 package nl.bigbrotter.androidclient.Helpers;
 
-/**
- * Created by Ricardo on 11-6-2018.
- */
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class AlertHelper {
+
+    protected static SweetAlertDialog dialog;
+
+    public static void progress(Context context, String title) {
+
+        if(dialog != null) {
+            dismiss(false);
+        }
+
+        SweetAlertDialog pDialog = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.setTitleText(title);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        dialog = pDialog;
+    }
+
+    public static void progress(Context context) {
+
+        progress(context, null);
+    }
+
+    public static void error(Context context, String title) {
+
+        if(dialog != null) {
+
+            dialog.changeAlertType(SweetAlertDialog.ERROR_TYPE);
+
+            if(title != null) {
+                dialog.setTitleText(title);
+            }
+        }
+        else {
+
+            SweetAlertDialog pDialog = new SweetAlertDialog(context, SweetAlertDialog.ERROR_TYPE);
+            pDialog.setTitleText(title);
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+            pDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+
+                @Override
+                public void onClick(SweetAlertDialog sweetAlertDialog) {
+
+                    dismiss();
+                }
+            });
+
+            dialog = pDialog;
+        }
+    }
+
+    public static void error(Context context) {
+
+        error(context, null);
+    }
+
+    public static void dismiss(boolean animated) {
+
+        if(dialog == null) {
+            return;
+        }
+
+        if(animated) {
+            dialog.dismissWithAnimation();
+        }
+        else {
+            dialog.dismiss();
+        }
+
+        dialog = null;
+    }
+
+    public static void dismiss() {
+
+        dismiss(true);
+    }
+
+    public static void showMessageOKCancel(Context context, String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(context)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 }
+
+
