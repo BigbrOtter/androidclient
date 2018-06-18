b75682c0121a3c555641db4460c68d04dbaedbce
Martijn Heeffer <Martijn.heeffer@hotmail.com>
2018-06-18 14:56:49 +0200

    Fix LoginActivity

--- app/src/main/java/nl/bigbrotter/androidclient/Controllers/LoginActivity.java
+++ app/src/main/java/nl/bigbrotter/androidclient/Controllers/LoginActivity.java
@@ -1,8 +1,12 @@
 package nl.bigbrotter.androidclient.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
 import org.json.JSONException;
 
@@ -10,46 +14,52 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.regex.Pattern;
 
 import nl.bigbrotter.androidclient.Helpers.DataHelper;
 import nl.bigbrotter.androidclient.R;
-import nl.bigbrotter.androidclient.View.FileChooser;
 
 public class LoginActivity extends AppCompatActivity {
-
-    FileChooser filechooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        filechooser = new FileChooser(LoginActivity.this);
-        filechooser.showDialog();
-        filechooser.setFileListener(new FileChooser.FileSelectedListener() {
-            @Override
-            public void fileSelected(final File file) {
+        new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(1)
+                .withFilter(Pattern.compile(".*\\.circle$")) // Filtering files and directories by file name using regexp
+                .withFilterDirectories(true) // Set directories filterable (false by default)
+                .withHiddenFiles(true) // Show hidden files and folders
+                .start();
+    }
 
-                String json = null;
-                try {
-                    //Get and read file
-                    InputStream is = new FileInputStream(file);
-                    int size = is.available();
-                    byte[] buffer = new byte[size];
-                    is.read(buffer);
-                    is.close();
-                    json = new String(buffer, "UTF-8");
-                    DataHelper.saveKeys(json, LoginActivity.this);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-                    //Testing
-                    Log.e("TESTTING", "Json: " + json);
-                    Log.e("TESTTING", "Public: " + DataHelper.getPublicKey(LoginActivity.this));
-                    Log.e("TESTTING", "Private: " + DataHelper.getPrivateKey(LoginActivity.this));
-                } catch (IOException | JSONException ex) {
-                    ex.printStackTrace();
-                }
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            File file = new File(filePath);
 
+            String json = null;
+            try {
+                InputStream is = new FileInputStream(file);
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                json = new String(buffer, "UTF-8");
+                Log.e("TESTTING", "Json: " + json);
+                DataHelper.saveKeys(json, LoginActivity.this);
+                Log.e("TESTTING", "Public: " + DataHelper.getPublicKey(LoginActivity.this));
+                Log.e("TESTTING", "Private: " + DataHelper.getPrivateKey(LoginActivity.this));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
+        }
     }
 }
