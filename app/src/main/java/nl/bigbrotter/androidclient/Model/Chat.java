d781c925cac4c0d4bd2f07fef55341a2d60adc21
KRademacher <KRademacher@users.noreply.github.com>
2018-06-21 15:18:33 +0200

    Added comments, cleaned up Chat

--- app/src/main/java/nl/bigbrotter/androidclient/Model/Chat.java
+++ app/src/main/java/nl/bigbrotter/androidclient/Model/Chat.java
@@ -4,27 +4,12 @@ package nl.bigbrotter.androidclient.Model;
  * Created by Ricardo on 11-6-2018.
  */
 
+//Chat messages
 public class Chat {
-
-    private String username;
     private String message;
-    private String timestamp;
-    private int streamer;
-
-    public Chat() {
-
-    }
 
     public Chat(String message) {
         this.message = message;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getMessage() {
@@ -33,21 +18,5 @@ public class Chat {
 
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStreamer() {
-        return streamer;
-    }
-
-    public void setStreamer(int streamer) {
-        this.streamer = streamer;
     }
 }
